723f5ced6f8b508b750a545c90f8946d68796fd6
puzzlefun32578 <pchelms3@cox.net>
2025-01-15 17:56:16 -0600

    Update Batman.ino

--- Batman.ino
+++ Batman.ino
@@ -1,7 +1,13 @@
-// Client Code
+// This is the code for the original Bat Caddy cart (Batman), and incorporates the GY 521 IMU on the MPU 6050 board
+
 #include "BLEDevice.h"
 #include <HardwareSerial.h>  //support Roboclaw interface
 #include "RoboClaw.h"        //  Use roboclaw commands
+#include <Adafruit_MPU6050.h>  //support IMU
+#include <Adafruit_Sensor.h>//support IMU
+#include <Wire.h>             //support IMU
+
+Adafruit_MPU6050 mpu;
 
 static BLEUUID    serviceUUID("ebdddb72-32f6-495f-aaf1-358ddba09f46");  //establish service "univeral unique identifier"
 static BLEUUID    controlCharUUID("ebdddb73-32f6-495f-aaf1-358ddba09f46");
@@ -15,6 +21,14 @@ static BLEAdvertisedDevice* myDevice;
 
 HardwareSerial serial(0);	  //HardwareSerial is a data class?  serial is an instantiation of the hardware  class. 
 RoboClaw roboclaw(&serial,10000);
+
+float current_turn_rate = 0; 
+float current_heading_error = 0;  //heading is always in radians and relative to the last requested orientation
+float scale_factor = 50;
+int time_of_last_correction = 0;
+float turn_correction = 0;
+float turn_bias = .02;   //when the cart is not moving it reads a turn rate of +.02 rads/sec. I will substract this from the measurement before using to adjust cart
+int previous_forward = 0;
 
 #define address 0x80  //0x means hex follows  80 is the default address of Roboclaw in hex
 
@@ -34,19 +48,31 @@ static void controlCallback(                           //
     int forward = command_str.substring(0,4).toInt();  //.toInt is a function that exists within String class.
     int right = command_str.substring(4,8).toInt();
 
-    int left_wheel = forward + right;  // -128  to +128
-    int right_wheel = forward - right;
+    //Serial.print("forward command from Robin is ");
+    //Serial.println(forward);
+    //Serial.print("right command from Robin is ");
+    //Serial.println(right);
+
+    if (abs(right)<5) {    //try to stop turning if there is no turn command given
+        right = 0;         //very small turn values are interpreted as a joystick bias and eliminated
+        turn_correction = current_heading_error * scale_factor;     //scale factor will be determined empirically; I think it will be negative so cart goes left if it is currently turning right
+      }
+      else { current_heading_error = 0;}    //rezero the heading error bucket if I am steering the cart
+    //}  
+    forward = constrain(forward, previous_forward - 5, previous_forward + 5);  // this should make stopping a little smoother.
+    previous_forward = forward;
+    int left_wheel = forward + right - (int)turn_correction;  // -128  to +128
+    int right_wheel = forward - right + (int)turn_correction;
 
     int left_wheel_cmd = left_wheel/2 + 64;   //roboclaw uses and unsigned 7 bit int for control; 0-63 is reverse; 65-127 forward; 64 is stop
     int right_wheel_cmd = right_wheel/2 + 64;
 
-    left_wheel_cmd = constrain(left_wheel_cmd, 0, 127);
-    right_wheel_cmd = constrain(right_wheel_cmd, 0, 127);
+    left_wheel_cmd = constrain(left_wheel_cmd, 0, 125);     //ensure the motor commands do not go out of range
+    right_wheel_cmd = constrain(right_wheel_cmd, 0, 125);
 
     Serial.print(left_wheel_cmd);
     Serial.print(", ");
     Serial.println(right_wheel_cmd);
-
 
     roboclaw.ForwardBackwardM1(address,left_wheel_cmd);
     roboclaw.ForwardBackwardM2(address,right_wheel_cmd);
@@ -127,13 +153,23 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 }; // MyAdvertisedDeviceCallbacks
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   serial.begin(38400, SERIAL_8N1, -1, -1);  //small s because this is an instance of the class hardwareserial
   Serial.println("Starting Arduino BLE Client application...");
   BLEDevice::init("");
 
   Serial.println("BLE initialized. Starting scan");
 
+  //while (!Serial)                   I dont think I need these 2 lines.  delete these is code works
+    //delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+    if (!mpu.begin()) {         //  initialize the IMU board
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+      }
+    }
+   
   // Retrieve a Scanner and set the callback we want to use to be informed when we
   // have detected a new device.  Specify that we want active scanning and start the
   // scan to run for 5 seconds.
@@ -143,10 +179,25 @@ void setup() {
   pBLEScan->setWindow(449);
   pBLEScan->setActiveScan(true);
   pBLEScan->start(5, false);
+
+  
 } // End of setup.
 
 // This is the Arduino main loop function.
 void loop() {
+
+  int frequency = 100;
+  for(int i = 0; i <frequency; i++){
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    current_heading_error = current_heading_error + g.gyro.z/frequency + .00018;   //integrate the z axis gyro to estimate current heading relative to desired heading
+    delay(10);     
+  }
+  
+    Serial.print(" current_heading_error is ");
+    Serial.print(current_heading_error);
+    Serial.println(" rad");
+
   if (doConnect == true) {
     if (connectToServer()) {
       Serial.println("Connected to the BLE Server.");
@@ -160,5 +211,5 @@ void loop() {
     BLEDevice::getScan()->start(0);
   }
   
-  delay(1000); // Delay a second between loops.
+  //delay(1000); // Delay a second between loops.     I don't need a delay here now since I am taking a second to read turnrate from MPU6050
 } // End of loop
